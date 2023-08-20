e0a1b891e536ac0be88e1e591bdd69ec789d61da
Kevin_was_here <xuyang4@ualberta.ca>
2023-08-20 17:07:32 -0600

    new and to be tested

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, jsonify
+import speech_recognition as sr
 
 app = Flask(__name__)
 
@@ -14,6 +15,19 @@ def send_message():
     if user_message:
         messages.append(user_message)
     return redirect('/')
+
+@app.route('/speech-to-text', methods=['POST'])
+def speech_to_text():
+    audio_file = request.files['audio_data'].read()
+    recognizer = sr.Recognizer()
+    audio_data = sr.AudioData(audio_file, 16000, 2)
+
+    try:
+        text = recognizer.recognize_google(audio_data)
+        messages.append(text)
+        return jsonify({'message': text})
+    except:
+        return jsonify({'error': 'Could not recognize the audio.'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
